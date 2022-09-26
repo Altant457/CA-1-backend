118641f642928262033da7bf8dc6f910cd59b639
Altant <28458922+Altant457@users.noreply.github.com>
2022-09-26 17:51:52 +0200

    Finally fixed the startcode

--- src/main/java/utils/CorsFilter.java
+++ src/main/java/utils/CorsFilter.java
@@ -1,8 +1,17 @@
+package utils;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.container.PreMatching;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+
 @PreMatching
 @Provider
 public class CorsFilter implements ContainerResponseFilter {
     @Override
-    public void filter( ContainerRequestContext requestCtx, ContainerResponseContext res )
+    public void filter(ContainerRequestContext requestCtx, ContainerResponseContext res )
     throws IOException {
         res.getHeaders().add("Access-Control-Allow-Origin", "*" );
         res.getHeaders().add("Access-Control-Allow-Credentials", "true" );
