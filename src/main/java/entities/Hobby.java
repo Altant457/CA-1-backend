e7dd6149ae845086d359a17fe412b977b7e6545e
sebastian1234 <slarvad@icloud.com>
2022-09-28 13:34:09 +0200

    added entities

---
+++ src/main/java/entities/Hobby.java
@@ -0,0 +1,55 @@
+package entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "hobby")
+public class Hobby {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String name;
+    private String description;
+
+    public Hobby() {
+    }
+
+    public Hobby(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Hobby{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
