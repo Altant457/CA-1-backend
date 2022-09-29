ebf55c423426b98d95c85fb748a7f9b1cb085267
Marie Fiskbæk <marie.fiskbak@gmail.com>
2022-09-29 16:17:40 +0200

    add simple JPA method

---
+++ src/main/java/dtos/PersonDTO.java
@@ -0,0 +1,74 @@
+package dtos;
+
+import entities.Person;
+
+import java.util.Objects;
+
+public class PersonDTO {
+    private Long id;
+    private String email, firstName, lastName;
+
+    public PersonDTO() {}
+
+    public PersonDTO(Person person) {
+        this.id = person.getId();
+        this.email = person.getEmail();
+        this.firstName = person.getFirstName();
+        this.lastName = person.getLastName();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonDTO{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PersonDTO personDTO = (PersonDTO) o;
+        return email.equals(personDTO.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email);
+    }
+}
